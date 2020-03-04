eb1b2cde7951cd1d2ef4b7ced206a60be8f15f8a
Millennium <glc2266@gmail.com>
2020-03-03 19:53:47 -0500

    v0.0
    Getting started.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 // Here's an example of what a sample page looks like.
 // See more: https://nextjs.org/learn/basics/getting-started/first-page
+import Layout from '../components/MyLayout';
 
-export default () => (
-  <div>
-    <p>Hello DSC!</p>
-  </div>
-)
+export default function Index() {
+  return (
+    <Layout>
+      <p>Hello DSC!</p>
+    </Layout>
+  );
+}
